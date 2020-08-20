e837dfb27755d438ae54479c50c98b0acdd59f0f
mityaycho <mityaycho@gmail.com>
2020-08-20 22:08:43 +0300

    install project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
