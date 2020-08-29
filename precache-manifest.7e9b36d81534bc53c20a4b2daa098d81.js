b18d662f3c104740278ddb6606da02aeb3a4e0f9
mityaycho <mityaycho@gmail.com>
2020-08-29 22:39:00 +0300

    Updates

---
+++ precache-manifest.7e9b36d81534bc53c20a4b2daa098d81.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a8b4c3ec75b0af8d8799009016041ca",
+    "url": "/test-task-sociaro-inc/index.html"
+  },
+  {
+    "revision": "d0b72c99912ce10a60c3",
+    "url": "/test-task-sociaro-inc/static/css/main.d7dfe339.chunk.css"
+  },
+  {
+    "revision": "38fa3b17dd8ba9f1ef95",
+    "url": "/test-task-sociaro-inc/static/js/2.95afde2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-task-sociaro-inc/static/js/2.95afde2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b72c99912ce10a60c3",
+    "url": "/test-task-sociaro-inc/static/js/main.e1920ed5.chunk.js"
+  },
+  {
+    "revision": "f56caa2728e05dc0b4dd",
+    "url": "/test-task-sociaro-inc/static/js/runtime-main.0dc8a17c.js"
+  },
+  {
+    "revision": "ba09ca19b0cf81b1344530acfbbfbb58",
+    "url": "/test-task-sociaro-inc/static/media/background-day.ba09ca19.png"
+  },
+  {
+    "revision": "55a8d67ed71631667b7b5dc9d1518ff7",
+    "url": "/test-task-sociaro-inc/static/media/background-night.55a8d67e.png"
+  }
+]);
