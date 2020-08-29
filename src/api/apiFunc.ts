fede3ad594fe527af4769f42b7c0439a8d7bcad5
mityaycho <mityaycho@gmail.com>
2020-08-29 22:02:04 +0300

    delete json cities

--- src/api/apiFunc.ts
+++ src/api/apiFunc.ts
@@ -1,14 +1,11 @@
 import { instance, apiKey } from './api';
-import citiesJson from '../assets/cities/city.list.json'
 
 export const api = {
 	getWeather(cityName: string) {
 			return instance.get(`weather?q=${cityName}&units=metric&appid=${apiKey}`);
 	},
 	getCities() {
-		// debugger
-		// const jsonCities = JSON.parse(citiesJson as any);
-		//  return jsonCities;
-		return citiesJson;
+		
+	
 	}
 };
